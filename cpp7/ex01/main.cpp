2c4e840a15f6c04b226c79656cc8b4d20e2ea732
Mohamed Korchi <korchimd@gmail.com>
2022-10-04 20:24:51 +0100

    clean

--- cpp7/ex01/main.cpp
+++ cpp7/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mkorchi <mkorchi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/28 19:51:52 by mkorchi           #+#    #+#             */
-/*   Updated: 2022/09/14 22:20:16 by mkorchi          ###   ########.fr       */
+/*   Updated: 2022/10/04 20:24:23 by mkorchi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,20 +40,14 @@ int	main( void )
 {
 	
 	int i[6] = {1, 2, 3, 6, 5, 0};
-	// float f[10] = {0.1f, 10, 5.5f, 9, 30.1f, 4.0f, 7, 132.0, 88.99, 5};
-	// std::string s[4] = {"sadf", "text2", "sadf", "test4"};
+	float f[10] = {0.1f, 10, 5.5f, 9, 30.1f, 4.0f, 7, 132.0, 88.99, 5};
+	std::string s[4] = {"sadf", "text2", "sadf", "test4"};
 
-	try {
-		std::cout << i[20] << std::endl;
-	} catch (std::exception &e){
-		std::cout << "error" << std::endl;
-		
-	}
-	// iter(i, 13, &fun);
-	// std::cout << "********************" << std::endl;
-	// iter(f, 10, &fun);
-	// std::cout << "********************" << std::endl;
-	// iter(s, 4, &fun);
+	iter(i, 13, &fun);
+	std::cout << "********************" << std::endl;
+	iter(f, 10, &fun);
+	std::cout << "********************" << std::endl;
+	iter(s, 4, &fun);
 	
 	return (0);
 }
