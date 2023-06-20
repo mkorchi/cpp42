810c4b47e0c89fe294b0ce12cc1baa1d894269a6
Mohamed Korchi <89984242+mkorchi@users.noreply.github.com>
2023-06-20 12:16:06 +0100

    Update Array.hpp, fixed

--- cpp7/ex02/Array.hpp
+++ cpp7/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: mkorchi <mkorchi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/03 16:01:44 by mkorchi           #+#    #+#             */
-/*   Updated: 2022/07/04 21:50:09 by mkorchi          ###   ########.fr       */
+/*   Updated: 2022/07/05 12:45:15 by mkorchi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 
 # define ARRAY_HPP
+# include "iostream"
 
 template < typename T >
 class Array
@@ -23,15 +24,66 @@ class Array
 		unsigned int _size;
 		
 	public:
-		Array( void );
-		virtual ~Array( void );
-		Array( Array const & src);
-		Array & operator=( Array const & rhs);
+	
+		Array( void )
+		{
+			std::cout << "Array created with 0 length" << std::endl;
+			this->_elems = new T[0];
+			this->_size = 0;			
+		}
+		
+		virtual ~Array( void )
+		{
+			std::cout << "Array deleted" << std::endl;
+			delete [] this->_elems;
+		}
+		
+		Array( Array const & src)
+		{
+			std::cout << "Array copy constructer" << std::endl;
+			this->_elems  = new T[src._size];
+			this->_size = src._size;
+			for( unsigned int i = 0; i < _size; i++)
+			{
+				this->_elems[i] = src._elems[i];
+			}
+		}
+		
+		Array & operator=( Array const & rhs)
+		{
+			std::cout << "Array operator = called" << std::endl;
+			if (this != rhs) {
+				delete [] this->_elems;
+				this->_elems = new T[rhs._size];
+				this->_size = rhs._size;
+				for( unsigned int i = 0; i < _size; i++)
+				{
+					this->_elems[i] = rhs._elems[i];
+				}
+			}
+			return *this;
+		}
 
-		Array( unsigned int n);
-		T & operator[](int index);
-		unsigned int size( void ) const;
-
+		Array( unsigned int n)
+		{
+			std::cout << "Array initialized with size " << n << std::endl;
+			this->_elems = new T[n];
+			this->_size = n;
+		}
+		
+		T & operator[](int index)
+		{
+			if (index < 0 || static_cast<unsigned int> (index) >= this->_size)
+			{
+				throw std::out_of_range("Exception: index out of bound");
+			}
+			return this->_elems[index];
+		}
+		
+		unsigned int size( void ) const
+		{
+			return this->_size;
+		}
 };
 
 
